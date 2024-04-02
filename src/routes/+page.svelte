99970895b508bc07b0ec7c190ce1bec14f1a820a
Paul Lessing <lessing.paul+github@gmail.com>
2024-04-02 16:58:33 +0700

    Improve styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,27 +15,80 @@
 	$: activeReminder = reminders[showReminder];
 
 	const onClickType = (value: ReminderType) => async () => {
+		showReminder = value;
+
 		if (value === 'none') {
 			return;
 		}
 
-		showReminder = value;
-
-		console.log('about to add', value);
+		// console.log('about to add', value);
 		await db.addEntry(new Date(), value);
-		console.log('done adding');
+		// console.log('done adding');
 
 		setTimeout(() => {
 			showReminder = 'none';
 		}, 5000);
 	};
+
+	let mealsThisSeating = Math.floor(Math.random() * 200);
+	let mealsToday = mealsThisSeating + Math.floor(Math.random() * 400);
+	let mealsTotal = mealsToday + Math.floor(Math.random() * 1000);
 </script>
 
-<h1>EMF Volunteer Kitchen</h1>
-<h2>Please select your role before taking a plate</h2>
-<button on:click={onClickType('volunteer')}>Volunteer</button>
-<button on:click={onClickType('orga')}>EMF Orga Member</button>
+<p>EMF Volunteer Kitchen</p>
+<h1>Eater Counter</h1>
+<p>Please select your role before taking a plate</p>
+<div class="eater-selection">
+	<button
+		class="eater-selection__choice eater-selection__choice--volunteer"
+		on:click={onClickType('volunteer')}>Vo&shy;lun&shy;teer</button
+	>
+	<button
+		class="eater-selection__choice eater-selection__choice--orga"
+		on:click={onClickType('orga')}>EMF Orga Member</button
+	>
+</div>
+<p>Meal no. #{mealsThisSeating + 1}</p>
+<p>Total meals served today: {mealsToday}</p>
+<p>Total meals served this EMF: {mealsTotal}</p>
 
 {#if activeReminder}
 	<p class="reminder">Thank you!<br />{activeReminder}</p>
+	<p><button on:click={onClickType('none')}>Next Person</button></p>
 {/if}
+
+<style lang="scss">
+	.eater-selection {
+		display: flex;
+
+		$flex-gutter: 5vw;
+		padding: 0 ($flex-gutter * 0.5);
+
+		&__choice {
+			flex: 100% 1 1;
+			height: 70vh;
+			font-size: 10vh;
+			text-wrap: wrap;
+			word-wrap: break-word;
+			padding: 2rem;
+
+			border: 0;
+			box-shadow: 0 0 16px 4px rgba(0, 0, 0, 0.3);
+
+			border-radius: 20px;
+
+			margin: 0 ($flex-gutter * 0.5);
+
+			// &:not(:last-child) {
+			// 	margin-right: 10vw;
+			// }
+
+			&--volunteer {
+				background: #ffff93;
+			}
+			&--orga {
+				background: #9891ff;
+			}
+		}
+	}
+</style>
